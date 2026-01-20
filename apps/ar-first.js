2b3bacb0af5dbf69091f1fa44da7b7f1b9a3257b
ArminPosadas <armin.posadas@uniat.edu.mx>
2026-01-20 11:37:06 -0600

    test3

--- apps/ar-first.js
+++ apps/ar-first.js
@@ -9,56 +9,70 @@ init();
 loadModel();
 animate();
 
-function init(){
+function init() {
     scene = new THREE.Scene();
-        camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
-        renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true});
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.xr.enabled = true;
-        document.body.appendChild(renderer.domElement);
-        document.body.appendChild(
-            ARButton.createButton(renderer, {
-                requiredFeatures: ['Test']
-            })
-        );
+
+    camera = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        0.01,
+        20
+    );
+
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.xr.enabled = true;
+
+    document.body.appendChild(renderer.domElement);
+    document.body.appendChild(
+        ARButton.createButton(renderer)
+    );
+
     const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     scene.add(light);
+
     controller = renderer.xr.getController(0);
     controller.addEventListener('select', onSelect);
     scene.add(controller);
-    window.addEventListener('resize', onWindowsResize);
+
+    window.addEventListener('resize', onWindowResize);
 }
 
-function loadModel(){
+function loadModel() {
     const loader = new GLTFLoader();
-    loader.load('models/Slime.glb', function(gltf) {
-    model = gltf.scene;
-    model.scale.set(0.1, 0.1, 0.1);
-    model.visible = false;
-    scene.add(model);
-});
+    loader.load('models/Slime.glb', function (gltf) {
+        model = gltf.scene;
+        model.scale.set(0.1, 0.1, 0.1);
+        model.visible = true;
+
+        model.position.set(0, 0, -1);
+
+        scene.add(model);
+    });
 }
 
-function onSelect(){
+function onSelect() {
     const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
     const material = new THREE.MeshStandardMaterial({ color: 0xf27f5d });
     const cube = new THREE.Mesh(geometry, material);
+
     cube.position.setFromMatrixPosition(controller.matrixWorld);
     cube.quaternion.setFromRotationMatrix(controller.matrixWorld);
+
     scene.add(cube);
 }
 
-function animate(){
+function animate() {
     renderer.setAnimationLoop(render);
 }
 
-function render(){
+function render() {
     renderer.render(scene, camera);
 }
 
-function onWindowsResize() {
+function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
-
