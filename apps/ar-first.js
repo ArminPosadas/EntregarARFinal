4184b3195be4cec3ab3e343968aae65dddd13f19
ArminPosadas <armin.posadas@uniat.edu.mx>
2026-01-20 11:32:19 -0600

    test2

--- apps/ar-first.js
+++ apps/ar-first.js
@@ -10,29 +10,22 @@ loadModel();
 animate();
 
 function init(){
-
     scene = new THREE.Scene();
-
         camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
-
         renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true});
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.xr.enabled = true;
         document.body.appendChild(renderer.domElement);
-
         document.body.appendChild(
             ARButton.createButton(renderer, {
                 requiredFeatures: ['Test']
             })
         );
-
     const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     scene.add(light);
-
     controller = renderer.xr.getController(0);
     controller.addEventListener('select', onSelect);
     scene.add(controller);
-
     window.addEventListener('resize', onWindowsResize);
 }
 
@@ -49,9 +42,7 @@ function loadModel(){
 function onSelect(){
     const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
     const material = new THREE.MeshStandardMaterial({ color: 0xf27f5d });
-
     const cube = new THREE.Mesh(geometry, material);
-
     cube.position.setFromMatrixPosition(controller.matrixWorld);
     cube.quaternion.setFromRotationMatrix(controller.matrixWorld);
     scene.add(cube);
